dee8836f035d6e1f3501f203355ce5c03c7fed9a
ehatl <67586660+ehatl@users.noreply.github.com>
2023-05-03 08:11:28 -0600

    Update S02_BWA2BAM.sh
    Change Spanish comment to English

--- S02_BWA2BAM.sh
+++ S02_BWA2BAM.sh
@@ -31,56 +31,56 @@ ${BWA} -t ${NT} ${GenomeREF} ${FILTERFQ}"/"${Cuatro} -o ${PATHBAM}"/"${SAMoutsR2
 
 ####### Cambiar de formato los SAM a BAM ########
 
-#Generando nombres de archivos de salida
+#Generating output file names
 BAMoutp=`echo ${SAMoutp} | sed 's/\.sam/\.bam/'`
 BAMoutsR1=`echo ${SAMoutsR1} | sed 's/\.sam/\.bam/'`
 BAMoutsR2=`echo ${SAMoutsR2} | sed 's/\.sam/\.bam/'`
 
 
-#Cambio de formato los SAM a BAM
+#Change format from SAM to BAM
 ${SAMTOOLS} view -bS --threads ${NT} ${PATHBAM}"/"${SAMoutp} -o ${PATHBAM}"/"${BAMoutp}
 ${SAMTOOLS} view -bS --threads ${NT} ${PATHBAM}"/"${SAMoutsR1} -o ${PATHBAM}"/"${BAMoutsR1}
 ${SAMTOOLS} view -bS --threads ${NT} ${PATHBAM}"/"${SAMoutsR2} -o ${PATHBAM}"/"${BAMoutsR2}
 
 
-###### Ordenar por coordenada los archivos BAM ########
+###### Sort by coordinate of BAM files ########
 
-#Cambio de nombre para generar archivos BAM sorted
+#Change names for sorted BAM output files
 BAMSORTp=`echo ${BAMoutp} | sed 's/\.bam/_sorted\.bam/'`
 BAMSORTsR1=`echo ${BAMoutsR1} | sed 's/\.bam/_sorted\.bam/'`
 BAMSORTsR2=`echo ${BAMoutsR2} | sed 's/\.bam/_sorted\.bam/'`
 
 
-#Ejecutar picard para ordenar
+#Run Picard to order
 ${Picard} SortSam I=${PATHBAM}"/"${BAMoutp} O=${PATHBAM}"/"${BAMSORTp} SO=coordinate
 ${Picard} SortSam I=${PATHBAM}"/"${BAMoutsR1} O=${PATHBAM}"/"${BAMSORTsR1} SO=coordinate
 ${Picard} SortSam I=${PATHBAM}"/"${BAMoutsR2} O=${PATHBAM}"/"${BAMSORTsR2} SO=coordinate
 
 
-##### Hacer merge de los archivos BAM ########
+##### Merge BAM files ########
 
-#Cambio de nombre para el archivo BAM merge
+#Change name for sort BAM file
 BAMoutm=`echo ${BAMSORTp} | sed 's/_paired_sorted\.bam/_merged\.bam/'`
 
-#Ejecutar Picard para generar el BAM merge
+#Run Picard for merge BAM 
 ${Picard} MergeSamFiles INPUT=${PATHBAM}"/"${BAMSORTp} INPUT=${PATHBAM}"/"${BAMSORTsR1} INPUT=${PATHBAM}"/"${BAMSORTsR2} OUTPUT=${PATHBAM}"/"${BAMoutm}
 
 
-###### Agregar readgroups al archivo BAM merge ######
+###### Add readgroups to the attached BAM file ######
 
-#Cambio de nombre para archivo BAM-RG
+#Change name for BAM-RG file 
 BAMoutRG=`echo ${BAMoutm} | sed 's/_merged\.bam/_merged-RG\.bam/'`
 
-#Generacion de ReadGroups (es necesario cambiar RGLB por fecha o quien lo realizo, 
-#RGPL en la plataforma que se hizo el experimento, siempre se crea el indice no mover.
+#Generation of ReadGroups (it is necessary to change RGLB by date or by who performed it)
+#RGPL on the platform where the experiment was done, the index is always created not moved.
 RGID=`basename ${Uno} | cut -d '_' -f 1`
 RGPU=`basename ${Uno} | cut -d '_' -f 3`
 RGSM=`basename ${Uno} | cut -d '_' -f 1,3`
 
-#Ejecutar Picard para agregar readgroups
-#El readgroup RGBL debe de ser remplazado para cada experimento
+#Run Picard to attached readgroups
+#The RGBL readgroup must be replaced for each experiment.
 ${Picard} AddOrReplaceReadGroups I=${PATHBAM}"/"${BAMoutm} O=${PATHBAM}"/"${BAMoutRG} RGID=${RGID} RGPU=${RGPU} RGLB=2021_ALf RGSM=${RGSM}  RGPL=ILLUMINA CREATE_INDEX=true
 
 
-#Eliminando archivos ya no necesarios
+#Deleting unneeded files
 #rm -r ${PATHBAM}"/"${SAMoutp} ${PATHBAM}"/"${SAMoutsR1} ${PATHBAM}"/"${SAMoutsR2} ${PATHBAM}"/"${BAMoutp} ${PATHBAM}"/"${BAMoutsR1} ${PATHBAM}"/"${BAMoutsR2} ${PATHBAM}"/"${BAMSORTp} ${PATHBAM}"/"${BAMSORTsR1} ${PATHBAM}"/"${BAMSORTsR2}
